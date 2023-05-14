6b17a9f8b36afbcf09d9019221f3ddf9524c4213
mancinilucas <mancini.lucasg@gmail.com>
2023-05-14 12:52:53 -0300

    Domain Model

---
+++ src/main/java/com/mancinilucas/dslist/entities/GameList.java
@@ -0,0 +1,53 @@
+package com.mancinilucas.dslist.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_game_list")
+public class GameList implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public GameList() {
+    }
+
+    public GameList(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof GameList gameList)) return false;
+        return Objects.equals(id, gameList.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
